9b6ba3ff425e92e323dce43b9a9e491b8e59d172
frorpi <francisco.ortola.piles@gmail.com>
2018-09-26 08:53:56 +0200

    subiendo archivos

---
+++ fichero.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Comprueba el estado y sube los cambios</title>
+  </head>
+  <body>
+    <?php
+
+    
+
+
+     ?>
+  </body>
+</html>
